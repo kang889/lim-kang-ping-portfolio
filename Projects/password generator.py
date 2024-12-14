f1d6f57e116633c96cb5bd329104e25c188c2145
kang889 <155519304+kang889@users.noreply.github.com>
2024-12-15 01:19:10 +0800

    Add files via upload

--- Projects/password generator.py
+++ Projects/password generator.py
@@ -1,11 +1,18 @@
 import random
 
+# Character groups
 characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+/?><'
+capletters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lowerletters = 'abcdefghijklmnopqrstuvwxyz'
+specialchar = '!@#$%^&*()_+'
+nums = '0123456789'
 
-password = ''
-
-for x in range(16):
-    password += random.choice(characters)
-
-
-print('Password:', password)
+# Keep generating until criteria are met
+while True:
+    password = ''.join(random.choices(characters, k=16))
+    if (any(char in capletters for char in password) and
+        any(char in lowerletters for char in password) and
+        any(char in specialchar for char in password) and
+        any(char in nums for char in password)):
+        print('Secure Password:', password)
+        break
